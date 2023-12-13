5f41ed7596ba38abdbaa539e375900416c978a85
xcy960815 <18763006837@163.com>
2023-12-13 14:06:08 +0800

    feat: 暂存提交

--- pages/analyse/components/charts/handler.ts
+++ pages/analyse/components/charts/handler.ts
@@ -2,85 +2,7 @@
  * @desc 逻辑处理
  */
 export const handler = () => {
-  const filterStore = useFilterStore()
-  const orderStore = useOrderStore()
-  const dimensionStore = useDimensionStore()
-  const groupStore = useGroupStore()
-  const chartStore = useChartStore()
-  /**
-   * @desc 分析参数
-   */
-  const analyseParams = computed(() => {
-    return {
-      filter: filterStore.getFilters,
-      order: orderStore.getOrders,
-      dimension: dimensionStore.getDimensions,
-      group: groupStore.getGroups,
-      chartType: chartStore.getChartType
-    }
-  })
-  /**
-   * @desc 图表推荐策略类
-   * @param {ChartStore.ChartState['chartType']} chartType
-   * @returns {string}
-   */
-  const chartSuggestStrategies = (
-    chartType: ChartStore.ChartState['chartType']
-  ) => {
-    const dimensions = dimensionStore.getDimensions
-    const groups = groupStore.getGroups
-    const chartNames = {
-      table: '表格',
-      interval: '柱状图',
-      line: '折线图',
-      pie: '饼图'
-    }
-    switch (chartType) {
-      case 'table':
-        if (dimensions.length > 0 || groups.length > 0) {
-          return ''
-        } else {
-          return `${chartNames[chartType]}至少需要一个值或者一个分组`
-        }
-      case 'interval':
-      case 'line':
-        if (dimensions.length > 0 && groups.length > 0) {
-          return ''
-        } else {
-          return `${chartNames[chartType]}至少需要一个值和一个分组`
-        }
-      case 'pie':
-        if (
-          dimensions.length === 1 &&
-          groups.length === 1
-        ) {
-          return ''
-        } else {
-          return `${chartNames[chartType]}只需要一个值和一个分组`
-        }
-      default:
-        return ''
-    }
-  }
-
-  /**
-   * @desc 获取分析数据
-   */
-  const getAnalyseData = () => {
-    /**
-     * @desc 前置校验先检查维度、分组是否有值
-     */
-    const chartType = chartStore.getChartType as ChartStore.ChartState['chartType']
-    const errorMessage = chartSuggestStrategies(chartType)
-    chartStore.setChartErrorMessage(errorMessage)
-    if (errorMessage) {
-      return
-    }
-    chartStore.setChartLoading(true)
-    setTimeout(() => {
-      chartStore.setChartLoading(false)
-    }, 500)
-  }
+  
   /**
    * @desc 图表开始渲染
    */
@@ -90,18 +12,6 @@ export const handler = () => {
    * @desc 图表结束渲染
    */
   const handleRenderChartEnd = () => {}
-
-  onMounted(() => {
-    getAnalyseData()
-  })
-
-  watch(
-    () => analyseParams.value,
-    () => {
-      getAnalyseData()
-    },
-    { deep: true }
-  )
 
   return {
     handleRenderChartStart,
--- server/api/employees/delete.delete.ts
+++ server/api/employees/delete.delete.ts
@@ -1,28 +1,28 @@
-import { EmployeesDao } from '../../database/employees';
+// import { EmployeesDao } from '../../database/employees';
 
 
-/**
- * @description 删除员工
- * @param {number} id 员工id
- * @return {Promise<ResponseModule.Response<EmployeesModule.EmployeesOptions | null>>}
- */
-export default defineEventHandler<Promise<ResponseModule.Response<EmployeesModule.EmployeesOptions>>>(
-  async (event) => {
-    try {
-      const { id } = getQuery(event);
-      const employeesDao = new EmployeesDao();
-      await employeesDao.deleteById(id as number);
-      return {
-        code: 200,
-        data: null,
-        message: 'success',
-      };
-    } catch (error: any) {
-      return {
-        code: 500,
-        data: null,
-        message: error.message,
-      };
-    }
-  },
-);
+// /**
+//  * @description 删除员工
+//  * @param {number} id 员工id
+//  * @return {Promise<ResponseModule.Response<EmployeesModule.EmployeesOptions | null>>}
+//  */
+// export default defineEventHandler<Promise<ResponseModule.Response<EmployeesModule.EmployeesOptions>>>(
+//   async (event) => {
+//     try {
+//       const { id } = getQuery(event);
+//       const employeesDao = new EmployeesDao();
+//       await employeesDao.deleteById(id as number);
+//       return {
+//         code: 200,
+//         data: null,
+//         message: 'success',
+//       };
+//     } catch (error: any) {
+//       return {
+//         code: 500,
+//         data: null,
+//         message: error.message,
+//       };
+//     }
+//   },
+// );
