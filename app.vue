be7435eade57a6e0d5ebcd68c817ed1ebe34c64c
xcy960815 <18763006837@163.com>
2025-06-16 10:08:12 +0800

    feat: 代码暂存

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
-  <!-- <InitialLoading /> -->
-  <NuxtPage />
+  <NuxtPage>
+    <!-- <InitialLoading /> -->
+  </NuxtPage>
 </template>
 
 <script lang="ts" setup>
-// import InitialLoading from '~/components/InitialLoading.vue'
+import InitialLoading from '~/components/InitialLoading.vue'
 </script>
