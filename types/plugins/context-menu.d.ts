35725a965fc155799708b19f83dcc96d179a294f
xcy960815 <18763006837@163.com>
2025-06-30 17:17:28 +0800

    feat: 代码优化

--- types/plugins/context-menu.d.ts
+++ types/plugins/context-menu.d.ts
@@ -2,48 +2,43 @@
  * @desc contextmenu directive and component definition
  */
 
-import type { DirectiveBinding } from 'vue'
+declare namespace ContextMenu {
+  type DirectiveBinding = import('vue').DirectiveBinding
+  /**
+   * @desc 右键菜单元素
+   */
+  export interface ContextMenuElement extends Element {
+    $contextmenuRefName?: string
+  }
+  /**
+   * @desc 触发方式
+   */
+  export type TriggerType = 'contextmenu' | 'click'
+  /**
+   * @desc 右键菜单配置项
+   */
+  export interface ContextMenuOtions {
+    triggerTypes: TriggerType[]
+    triggerEventHandler: (evt: Event) => void
+  }
 
-declare global {
-  namespace ContextMenu {
-    /**
-     * @desc 右键菜单元素
-     */
-    export interface ContextMenuElement extends Element {
-      $contextmenuRefName?: string
-    }
-    /**
-     * @desc 触发方式
-     */
-    export type TriggerType = 'contextmenu' | 'click'
-    /**
-     * @desc 右键菜单配置项
-     */
-    export interface ContextMenuOtions {
+  export interface ContextMenuBinding
+    extends DirectiveBinding<{
       triggerTypes: TriggerType[]
-      triggerEventHandler: (evt: Event) => void
-    }
+    }> {}
 
-    export interface ContextMenuBinding
-      extends DirectiveBinding<{
-        triggerTypes: TriggerType[]
-      }> {}
+  export interface ContextMenuInstance {
+    initContextMenuEvent(
+      contextMenuElement: ContextMenuElement,
+      options?: InitContextMenuOptions
+    ): void
 
-    export interface ContextMenuInstance {
-      initContextMenuEvent(
-        contextMenuElement: ContextMenuElement,
-        options?: InitContextMenuOptions
-      ): void
+    removeContextMenuEvent(
+      contextMenuElement: ContextMenuElement
+    ): void
+  }
 
-      removeContextMenuEvent(
-        contextMenuElement: ContextMenuElement
-      ): void
-    }
-
-    export interface InitContextMenuOptions {
-      triggerTypes?: TriggerType | TriggerType[]
-    }
+  export interface InitContextMenuOptions {
+    triggerTypes?: TriggerType | TriggerType[]
   }
 }
-
-export {}
