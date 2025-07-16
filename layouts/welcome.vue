87e68c135f234a60ea6fd9fd407cbc20c15be252
xcy960815 <18763006837@163.com>
2025-07-16 22:31:34 +0800

    feat: 代码暂存

--- layouts/welcome.vue
+++ layouts/welcome.vue
@@ -11,23 +11,6 @@
 defineOptions({
   name: 'WelcomeLayout'
 })
-
-// 初始化数据
-const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-const fontColors = [
-  '#33B5E5',
-  '#0099CC',
-  '#AA66CC',
-  '#9933CC',
-  '#99CC00',
-  '#669900',
-  '#FFBB33',
-  '#FF8800',
-  '#FF4444',
-  '#CC0000'
-]
-const fontSizeMultiplier = 1
-const setIntervalId = ref<number | null>(null)
 </script>
 
 <style lang="scss" scoped>
