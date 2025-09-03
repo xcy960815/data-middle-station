364ddcfa6494eaf7114edc0023427c9cc6c6eb5a
xcy960815 <18763006837@163.com>
2025-09-03 22:47:07 +0800

    feat: add root redirect page and client-only Monaco editor loading\n\n- Add pages/index.vue to redirect '/' to '/welcome'\n- Update components/monaco-editor/index.vue for client-only dynamic import\n- Add pages/monaco-editor/ route and examples

---
+++ pages/monaco-editor/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <div class="monaco-editor-container">
+      <MonacoEditor />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.monaco-editor-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
