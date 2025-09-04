246ee7a1ee3517bbe7a5d1252bf362def0745155
xcy960815 <18763006837@163.com>
2025-09-04 23:47:15 +0800

    feat(monaco-editor): add Monaco SQL editor component with autocompletion and demo page

--- components/monaco-editor/index.vue
+++ components/monaco-editor/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <ClientOnly>
-    <div ref="monacoEditorDom" class="monaco-editor-dom"></div>
-  </ClientOnly>
+  <div ref="monacoEditorDom" class="monaco-editor-dom"></div>
 </template>
 <script lang="ts" setup>
 import * as monaco from 'monaco-editor'
@@ -12,8 +10,9 @@ import type * as MonacoNS from 'monaco-editor/esm/vs/editor/editor.api'
 // import "monaco-editor/esm/vs/editor/contrib/hover/hover";
 
 import 'monaco-editor/esm/vs/basic-languages/css/css.contribution'
-import 'monaco-editor/esm/vs/basic-languages/xml/xml.contribution'
 import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution'
+import 'monaco-editor/esm/vs/basic-languages/sql/sql.contribution'
+import 'monaco-editor/esm/vs/basic-languages/xml/xml.contribution'
 
 /* 语法联想模块 */
 import { SqlSnippets } from './snippets'
@@ -44,14 +43,14 @@ const props = defineProps({
 
   /* 编译器的width */
   width: {
-    type: Number,
-    default: () => 0
+    type: [Number, String],
+    default: () => '100%'
   },
 
   /* 编译器的高 */
   height: {
-    type: Number,
-    default: () => 100
+    type: [Number, String],
+    default: () => '100%'
   },
 
   /* 编译器配置项 */
@@ -69,14 +68,29 @@ const props = defineProps({
 
 const emits = defineEmits(['update:modelValue'])
 
-const monacoEditorDom = ref<HTMLDivElement>()
+/**
+ * @desc monacoEditorDom
+ */
+const monacoEditorDom = ref<HTMLDivElement | null>(null)
 
-/* monacoEditor 实例 */
+/**
+ * @desc monacoEditor
+ */
 const monacoEditor = ref<MonacoNS.editor.IStandaloneCodeEditor | null>(null)
 
+/**
+ * @desc completionItemProvider
+ */
 const completionItemProvider = ref<MonacoNS.IDisposable>()
 
-// 编译器的默认配置
+/**
+ * @desc resizeObserver for percent-based sizing
+ */
+const resizeObserver = ref<ResizeObserver | null>(null)
+
+/**
+ * @desc 编译器的默认配置
+ */
 const monacoEditorDefaultOption: MonacoNS.editor.IStandaloneEditorConstructionOptions = {
   acceptSuggestionOnCommitCharacter: false,
   suggestSelection: 'first',
@@ -102,6 +116,8 @@ const monacoEditorDefaultOption: MonacoNS.editor.IStandaloneEditorConstructionOp
 watch(
   () => props.modelValue,
   (newSql: string) => {
+    console.log('newSql', newSql, monacoEditor.value)
+
     const hasTextFocus = monacoEditor.value?.hasTextFocus()
     if (!hasTextFocus) toRaw(monacoEditor.value)?.setValue(newSql)
   }
@@ -130,19 +146,61 @@ watch(
  * @desc 设置 编译器宽高
  */
 const setMonacoEditorStyle = () => {
+  const parentElement = monacoEditorDom.value?.parentElement
+  if (!parentElement) return
+  const parentElementBoundingClientRect = parentElement.getBoundingClientRect()
   // 获取 monacoEditorDom 节点的父节点
-  const parentElementWidth = window.getComputedStyle((monacoEditorDom.value as HTMLDivElement).parentElement!).width
-  const parentElementWidthNumber = Number(parentElementWidth.substring(0, parentElementWidth.length - 2))
+  const parentElementWidth = parentElementBoundingClientRect.width
+  const parentElementHeight = parentElementBoundingClientRect.height
+
+  let editorWidth: number = 0
+  let editorHeight: number = 0
+  // 如果是百分比 就取父节点宽高
+  if (typeof props.width === 'string' && props.width.endsWith('%')) {
+    editorWidth = (parentElementWidth * Number(props.width.replace('%', ''))) / 100
+  } else if (typeof props.width === 'number') {
+    editorWidth = props.width
+  } else {
+    editorWidth = parentElementWidth
+  }
+  if (typeof props.height === 'string' && props.height.endsWith('%')) {
+    editorHeight = (parentElementHeight * Number(props.height.replace('%', ''))) / 100
+  } else if (typeof props.height === 'number') {
+    editorHeight = props.height
+  } else {
+    editorHeight = parentElementHeight
+  }
+
   toRaw(monacoEditor.value)?.layout({
-    width: props.width ? props.width : parentElementWidthNumber,
-    height: props.height
+    width: editorWidth,
+    height: editorHeight
   })
+}
+
+/**
+ * @desc 初始化 ResizeObserver，当使用百分比宽高时监听父容器变化
+ */
+const initResizeObserver = () => {
+  const usesPercentWidth = typeof props.width === 'string' && props.width.endsWith('%')
+  const usesPercentHeight = typeof props.height === 'string' && props.height.endsWith('%')
+  if (!usesPercentWidth && !usesPercentHeight) return
+
+  const parentElement = monacoEditorDom.value?.parentElement
+  if (!parentElement) return
+  if (typeof window !== 'undefined' && 'ResizeObserver' in window) {
+    resizeObserver.value = new ResizeObserver(() => {
+      setMonacoEditorStyle()
+    })
+    resizeObserver.value.observe(parentElement)
+  }
 }
 
 /**
  * @desc 初始化 editor
  */
 const initEditor = () => {
+  console.log('initEditor')
+
   const sqlSnippets = new SqlSnippets(props.customKeywords, props.databaseOptions)
   completionItemProvider.value = monaco.languages.registerCompletionItemProvider('sql', {
     // 提示的触发字符
@@ -168,7 +226,10 @@ const initEditor = () => {
     emits('update:modelValue', toRaw(monacoEditor.value!).getValue())
   })
 }
-/* 重置 编译器 内容 */
+
+/**
+ * 重置 编译器 内容
+ */
 const resetEditor = () => {
   toRaw(monacoEditor.value)?.setValue('')
 }
@@ -177,12 +238,14 @@ onMounted(() => {
   // 解决通过dialog 弹出的组件 无法正常渲染的问题
   nextTick(() => {
     initEditor()
+    initResizeObserver()
   })
 })
 
 // 离开时销毁
 onBeforeUnmount(() => {
   completionItemProvider.value?.dispose()
+  resizeObserver.value?.disconnect()
 })
 
 defineExpose({
